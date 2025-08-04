50785e4d57aa55eeff43159e515960b472f5299f
Daniil Kaminskyi <deniil019@gmail.com>
2025-08-04 17:32:48 +0200

    chore: update image domains in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["convex.dev", "next-mallard-647.convex.cloud"],
+    domains: [
+      "convex.dev",
+      "next-mallard-647.convex.cloud",
+      "confident-cobra-406.convex.cloud",
+    ],
   },
 };
 
